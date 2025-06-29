448ebf6ed2eea5d020488e73fa5dbd9d92dbd0cc
ShanDewage <shandias07@gmail.com>
2025-06-29 23:46:33 +0530

    add colors typography, and create about contact pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        accent: "var(--accent)",
+        border: "var(--border)",
+        muted: "var(--muted-text)",
+        card: "var(--card-bg)",
+        link: "var(--link)",
+      },
+      fontFamily: {
+        sacramento: ["Sacramento", "cursive"],
+      },
+    },
   },
-  darkMode: 'class', // Enable class-based dark mode
+  darkMode: "class", // Enable class-based dark mode
   plugins: [],
-}
+};
